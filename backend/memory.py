0f6658fa297ec5e012fd59c456110cacf5883835
Amirmahdi Ghodsi <amirmahdig138@gmail.com>
2023-06-27 21:27:00 +0330

    Add memory

---
+++ backend/memory.py
@@ -0,0 +1,14 @@
+from register import Register
+
+class Memory:
+    def __init__(self, word_size, capacity):
+        self.ram = []
+        for i in range(capacity):
+            reg = Register(word_size)
+            self.ram.append(reg)
+
+    def __getitem__(self, item):
+        return self.ram[item]
+
+    def __setitem__(self, key, value):
+        self.ram[key] = value
