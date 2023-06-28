4988f83400eddb20dbb5a7aa84d4b69f3cf1c3e3
Mobin Ghoveoud <mobin.sgh@gmail.com>
2023-06-28 16:01:55 +0330

    Optimize micro assembler

--- backend/assembler/micro.py
+++ backend/assembler/micro.py
@@ -1,56 +1,56 @@
-
-
 class MicroprogramAssembler:
+    f1 = {
+        'NOP': '000',
+        'ADD': '001',
+        'CLRAC': '010',
+        'INCAC': '011',
+        'DRTAC': '100',
+        'DRTAR': '101',
+        'PCTAR': '110',
+        'WRITE': '111'
+    }
+    f2 = {
+        'SUB': '001',
+        'OR': '010',
+        'AND': '011',
+        'READ': '100',
+        'ACTDR': '101',
+        'INCDR': '110',
+        'PCTDR': '111'
+    }
+    f3 = {
+        'XOR': '001',
+        'COM': '010',
+        'SHL': '011',
+        'SHR': '100',
+        'INCPC': '101',
+        'ARTPC': '110'
+    }
+    conditions = {
+        'U': '00',
+        'I': '01',
+        'S': '10',
+        'Z': '11'
+    }
+    branch = {
+        'JMP': '00',
+        'CALL': '01',
+        'RET': '10',
+        'MAP': '11'
+    }
+
     def __init__(self, code: str):
         self.lines = code.split('\n')
         for i in range(len(self.lines)):
             self.lines[i] = self.lines[i].split()
-        self.f1 = {
-            'NOP': '000',
-            'ADD': '001',
-            'CLRAC': '010',
-            'INCAC': '011',
-            'DRTAC': '100',
-            'DRTAR': '101',
-            'PCTAR': '110',
-            'WRITE': '111'
-        }
 
-        self.f2 = {
-            'SUB': '001',
-            'OR': '010',
-            'AND': '011',
-            'READ': '100',
-            'ACTDR': '101',
-            'INCDR': '110',
-            'PCTDR': '111'
-        }
-
-        self.f3 = {
-            'XOR': '001',
-            'COM': '010',
-            'SHL': '011',
-            'SHR': '100',
-            'INCPC': '101',
-            'ARTPC': '110'
-        }
-
-        self.conditions = {
-            'U': '00',
-            'I': '01',
-            'S': '10',
-            'Z': '11'
-        }
-
-        self.branch = {
-            'JMP': '00',
-            'CALL': '01',
-            'RET': '10',
-            'MAP': '11'
-        }
+    def assemble(self):
+        self.label_dict = self.get_label_table()
+        self.res_dict = self.get_memory_words()
 
     def get_label_table(self):
         res_dict = {}
+
         current_address = 0
         for i in range(len(self.lines)):
             if self.lines[i][0] == 'ORG':
@@ -58,12 +58,12 @@ class MicroprogramAssembler:
             if self.lines[i][0][-1] == ':':
                 res_dict.__setitem__(self.lines[i][0].replace(':', ''), current_address)
             current_address += 1
-        print(res_dict)
+
         return res_dict
 
     def get_memory_words(self):
-        labels = self.get_label_table()
         res_dict = {}
+
         current_address = 0
         for i in range(len(self.lines)):
             word = list('0' * 20)
@@ -86,15 +86,18 @@ class MicroprogramAssembler:
                         word[9: 11] = self.conditions[self.lines[i][j]]
                     if self.lines[i][j] in self.branch:
                         word[11: 13] = self.branch[self.lines[i][j]]
-                    if self.lines[i][j] in labels:
-                        bin_address = bin(labels[self.lines[i][j]])[2:]
+                    if self.lines[i][j] in self.label_dict:
+                        bin_address = bin(self.label_dict[self.lines[i][j]])[2:]
                         word[13: 20] = bin_address.zfill(7)
                     if self.lines[i][j] == 'NEXT':
                         bin_address = bin(current_address + 1)[2:]
                         word[13: 20] = bin_address.zfill(7)
+
             res_dict.__setitem__(current_address, word)
             current_address += 1
+
         return res_dict
+
 
 if __name__ == "__main__":
     code = '''ORG 0
@@ -123,5 +126,8 @@ INDRCT: READ U JMP NEXT
         DRTAR U RET'''
 
     a = MicroprogramAssembler(code)
-    b = a.get_memory_words()
-    print(b)
+    a.assemble()
+
+    print(a.label_dict)
+    for i in a.res_dict:
+        print(i, a.res_dict[i])
